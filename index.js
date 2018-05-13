e8b2e908a7fe4f9a2e03db2775a0cf87663aa178
Prashant Farkya <prfarkya@in.ibm.com>
2018-05-14 00:40:30 +0530

    more specific towards cloud

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
