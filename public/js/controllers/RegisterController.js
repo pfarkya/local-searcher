f79d33a6babbecdedc8608b8c658e1ffd51246cf
sonukushwaha <sonu230392@gmail.com>
2018-09-23 16:12:28 +0530

    new code

--- public/js/controllers/RegisterController.js
+++ public/js/controllers/RegisterController.js
@@ -10,14 +10,15 @@ foodMeApp.controller('RegisterController', function RegisterController($scope,$l
 	$scope.loginpage = function() {
 		 $http({
 				method : "post",
-				url : "/add_user",
-				data : { email : "sonu2303@gmail.com" , Passwoed: "sonu2303" }
+				url : "/process_post",
+				data : { 'body' : { 'first_name' : 'sonu' , 'last_nmae' : 'kushwaha' }}
 			}).then(function mySuccess(response) {
 				$scope.myWelcome = response.data;
 				alert($scope.myWelcome);
 				$location.url('/login');
 			}, function myError(response) {
 				$scope.myWelcome = response.statusText;
+				alert("ssdfwew");
 				alert($scope.myWelcome);
 			});
 		
